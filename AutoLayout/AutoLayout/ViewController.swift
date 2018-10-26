62e971d0ee6fd990856a13df4310aefda6a07f6c
Medi Assumani <mediassumani49@gmail.com>
2018-10-26 12:54:50 -0700

    changes

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -10,39 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpView()
+        
+       
+       setUpPurpleView()
+       setUpBlue()
+        
+        
+        
     }
     
-    fileprivate func setUpView(){
-        
-        let purpleView = UIView()
-        let blueView = UIView()
-        
+    // This functions sets up the contraints of
+    func setUpPurpleView(){
         
         // Constraints and properties for the purple view
-        
+        let purpleView = UIView()
         purpleView.backgroundColor = UIColor.purple
         purpleView.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(purpleView)
         // Leading constraint
         let purpleViewLeadingConstraint = NSLayoutConstraint(item: purpleView,
-                                                   attribute: .leading,
-                                                   relatedBy: .equal,
-                                                   toItem: self.view,
-                                                   attribute: .leading,
-                                                   multiplier: 1,
-                                                   constant: 20)
+                                                             attribute: .leading,
+                                                             relatedBy: .equal,
+                                                             toItem: self.view,
+                                                             attribute: .leading,
+                                                             multiplier: 1,
+                                                             constant: 20)
         
         // Trailing constraint
         let purpleViewTrailingContraint = NSLayoutConstraint(item: purpleView,
-                                                    attribute: .trailing,
-                                                    relatedBy: .equal,
-                                                    toItem: self.view,
-                                                    attribute: .trailing,
-                                                    multiplier: 1,
-                                                    constant: 20)
+                                                             attribute: .trailing,
+                                                             relatedBy: .equal,
+                                                             toItem: self.view,
+                                                             attribute: .trailing,
+                                                             multiplier: 0.5,
+                                                             constant: 0)
         
         // Top Constraint
         let purpleViewTopConstraint = NSLayoutConstraint(item: purpleView,
@@ -55,9 +60,57 @@ class ViewController: UIViewController {
         
         
         // Bottom Constraint
-     
-        self.view.addSubview(purpleView)
+        let purpleViewBottomConstraint = NSLayoutConstraint(item: purpleView,
+                                                            attribute: .bottom,
+                                                            relatedBy: .equal,
+                                                            toItem: self.view,
+                                                            attribute: .bottom,
+                                                            multiplier: 1,
+                                                            constant: -50)
+        self.view.addConstraints([purpleViewLeadingConstraint, purpleViewTrailingContraint, purpleViewTopConstraint,purpleViewBottomConstraint])
+        
+    }
+    
+    func setUpBlue(){
+
+        let blueView = UIView()
+        blueView.backgroundColor = UIColor.cyan
         self.view.addSubview(blueView)
+
+        // Leading Constraint
+        let blueLeadingConstraint = NSLayoutConstraint(item: blueView,
+                                                       attribute: .leading,
+                                                       relatedBy: .equal,
+                                                       toItem: self.view,
+                                                       attribute: .leading,
+                                                       multiplier: 1,
+                                                       constant: 1)
+        
+        // Trailing Constraint
+        let blueTrailingConstraint = NSLayoutConstraint(item: blueView,
+                                                        attribute: .trailing,
+                                                        relatedBy: .equal,
+                                                        toItem: self.view,
+                                                        attribute: .trailing,
+                                                        multiplier: 1,
+                                                        constant: 1)
+        
+        let blueTopConstraint = NSLayoutConstraint(item: blueView,
+                                                   attribute: .top,
+                                                   relatedBy: .equal,
+                                                   toItem: self.view,
+                                                   attribute: .top,
+                                                   multiplier: 1,
+                                                   constant: 1)
+        
+        let blueBottomConstraint = NSLayoutConstraint(item: blueView,
+                                                      attribute: .bottom,
+                                                      relatedBy: .equal,
+                                                      toItem: self.view,
+                                                      attribute: .bottom,
+                                                      multiplier: 1,
+                                                      constant: 1)
+        self.view.addConstraints([blueTopConstraint, blueBottomConstraint, blueLeadingConstraint, blueTrailingConstraint])
     }
     
 }
