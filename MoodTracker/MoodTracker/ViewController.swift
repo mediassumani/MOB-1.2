7fadc8c233fae47f6ff4cceb7780c3d29131b4b3
Medi Assumani <mediassumani49@gmail.com>
2018-11-08 14:52:00 -0800

    New project mood tracker

---
+++ MoodTracker/MoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Medi Assumani on 11/8/18.
+//  Copyright © 2018 Medi Assumani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
